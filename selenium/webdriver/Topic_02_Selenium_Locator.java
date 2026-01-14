caa73e4cea7a0285a87d7510f58f0fa1f20a6608
Tran Van The <thetran1352003@gmail.com>
2026-01-15 00:12:30 +0700

    Add Lesson about Topic 02 Locator

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -2,25 +2,30 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Topic_02_Selenium_Locator {
     //1 - Setup: OS/ Browser/Web/Page/Data/Variable/Object/...
     WebDriver driver;
+
+    String fullName = "Selenium Locator";
     @BeforeClass
 
     public void initialBrowser(){
         // Mở Browser
         driver = new FirefoxDriver();
         // Mở App
-        driver.get("https://demo.nopcommerce.com/login");
+        driver.get("https://demo.nopcommerce.com/register");
     }
     @Test
     //2 - Action/ Execute: Tương tác lên element nào/ nhập liệu/ verify/...
-    public void TC_01_Register(){
+    public void TC_01_ID() throws InterruptedException {
         // Tương tác lên Email
         //<input class="email" autofocus="" type="email" data-val="true" data-val-regex="Wrong email"
         // data-val-required="Please enter your email"
@@ -35,16 +40,97 @@ public class Topic_02_Selenium_Locator {
 
         //Tương tác lên Email Address
         // 8 loại locator để tìm Email này
-        driver.findElement(By.id("email")).sendKeys("email");
+        driver.findElement(By.id("small-searchterms")).sendKeys("Macbook");
+        Thread.sleep(3000);
+
+        driver.findElement(By.id("FirstName")).sendKeys("Automation");
+        Thread.sleep(3000);
+        //Tìm 1 element
+//        WebElement emailTextbox = driver.findElement(By.cssSelector(""));
+//        emailTextbox.clear();
+//        emailTextbox.sendKeys();
+//        emailTextbox.isDisplayed();
+//        // 1 - Thao tác lên luôn (dùng 1 lần )
+//        driver.findElement(By.cssSelector("a")).click();
+//
+//        //Tìm nhiều elements
+//        List<WebElement> textBoxes = driver.findElements(By.cssSelector(""));
+
     }
     @Test
-    public void TC_02_Login(){
+    public void TC_02_Class() throws InterruptedException {
+        //Nó không lấy hết toàn bộ giá trị nếu có khoảng trắng
+        driver.findElement(By.className("register-next-step-button")).click();
+        Thread.sleep(3000);
+    }
+    @Test
+    public void TC_03_Name() throws InterruptedException {
+        driver.findElement((By.name("Email"))).sendKeys("thetran1352003@gmail.com");
+        Thread.sleep(3000);
+    }
+    @Test
+    public void TC_04_Link_Text(){
+        // Chỉ làm việc với link
+        // Thẻ a có thuộc tính href
+        //Phải lấy hết toàn bộ text
+        driver.findElement((By.linkText("Register")));
+        driver.findElement((By.linkText("Log in")));
+        driver.findElement((By.linkText("Wishlist")));
+    }
 
+    @Test
+    public void TC_05_Partial_Link_Text(){
+        // Chỉ làm việc với element là link
+        // Có thể lấy toàn bộ text hoặc 1 phần
+        driver.findElement(By.partialLinkText("Register"));
+
+        driver.findElement(By.partialLinkText("Digital"));
+
+        driver.findElement(By.partialLinkText("downloads"));
+    }
+
+    @Test
+    public void TC_06_CSS(){
+        driver.findElement(By.cssSelector("input#Company"));
+        driver.findElement(By.cssSelector("#Company"));
+        driver.findElement(By.cssSelector("input[id='Company']"));
+
+        driver.findElement(By.cssSelector("button.register-next-step-button]"));
+        driver.findElement(By.cssSelector("button[class='button-1 register-next-step-button']"));
+
+        driver.findElement(By.cssSelector("a[href*='register?']"));
+        driver.findElement(By.cssSelector("a[href*='login?']"));
+
+        driver.findElement(By.cssSelector("a"));
+        driver.findElement(By.cssSelector("button"));
+        driver.findElement(By.cssSelector("input"));
+    }
+    @Test
+    public void TC_07_Tagname(){
+        // Tên thẻ HTML
+        // Tìm tất cả các element giống nhau
+        driver.findElements(By.tagName("button"));
+
+        driver.findElements(By.tagName("input"));
+
+        driver.findElements(By.tagName("label"));
+    }
+    @Test
+    public void TC_08_XPath(){
+        driver.findElement(By.xpath("//input[@id='small-searchterms']"));
+        driver.findElement(By.xpath("//input[@id='Password']"));
+        driver.findElement(By.xpath("//input[@id='Company']"));
+
+        driver.findElement(By.xpath("//a[text()='Register']"));
+        driver.findElement(By.xpath("//a[contains(text()='Shipping')]"));
     }
     @AfterClass
     //3 - Clean : Delete data test/ account/ close browser
     public void cleanBrowser(){
         driver.quit();
+    }
+    public void clickToElement(String locator, int index){
+        driver.findElement(By.cssSelector("a")).click();
     }
 
 }
